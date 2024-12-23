4a68fb00360fbc682033cc10ec75a52ac6fda772
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-23 18:02:01 +0700

    chore: tipe data primitif

---
+++ tests/type-data.test.ts
@@ -0,0 +1,10 @@
+describe("Data Type", () => {
+  "should must declare";
+  it("should must declare", () => {
+    const name: string = "Hello Dwi";
+    const balance: number = 1000;
+    const isVip: boolean = true;
+
+    console.info(name, balance, isVip);
+  });
+});
